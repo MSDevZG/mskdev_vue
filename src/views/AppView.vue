71a4bca6f9a7a793349489b65cde73364fd1d580
MSKDevZG <m.skrzypka@gmail.com>
2023-09-03 14:34:43 +0200

    update

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="xx">{{ this.game.name }}</div>
+  <div class="app">
+    <div class="title">{{ game.name }}</div>
+    <div class="privacy" v-html="game.policy_privacy" />
+  </div>
 </template>
 
 <script>
@@ -20,4 +23,24 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app {
+  @apply max-w-7xl mx-auto flex flex-col;
+
+  .privacy {
+    @apply p-5 text-white;
+
+    &:deep(h1) {
+      @apply font-bold text-4xl py-2;
+    }
+
+    &:deep(h2) {
+      @apply text-2xl mt-2 font-bold;
+    }
+
+    &:deep(p) {
+      @apply text-xl py-2;
+    }
+  }
+}
+</style>
